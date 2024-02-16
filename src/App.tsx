7a4a404d4e0f964d443b4b3fa0a1267b4893ff36
Shirley <shirleylaurette1@gmail.com>
2024-02-16 15:18:56 +0100

    display

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
+import Header from "./components/Header";
+import AllCountries from "./components/AllCountries";
+import CountryDetail from "./components/CountryDetail";
+import { useRef, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-
-  return (
-    <>
-     
-      <h1 className="text-xs underline">Vite + React</h1>
-     
-    </>
-  )
+interface AppProps {
+  // Add any prop types necessary for App
 }
 
-export default App
+function App({ /* props */ }: AppProps) {
+  const appRef = useRef<HTMLDivElement>(null);
+
+  // State for home button (the left side of header), so that AllCountries component can track the state
+  // for useEffect dependency
+  const [goHome, setGoHome] = useState(0);
+
+  return (
+    <Router>
+      <div
+        ref={appRef}
+        className="App dark-mode font-primary-font min-h-screen w-full bg-main-background text-main-color-text"
+      >
+        {/* HEADER COMPONENT */}
+        <Header appRef={appRef} setGoHome={setGoHome} goHome={goHome} />
+
+        <div className="content">
+          <Routes>
+            {/* ALL COUNTRIES PAGE */}
+            <Route path="/" element={<AllCountries goHome={goHome} />} />
+
+            {/* COUNTRY DETAIL PAGE */}
+            <Route path="/:countryName" element={<CountryDetail />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
